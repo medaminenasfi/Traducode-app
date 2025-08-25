c9d682196b23d94f020d19bcff4be952ad6cd9e4
Mohamed Amine Nasfi <152500239+medaminenasfi@users.noreply.github.com>
2025-08-25 15:50:00 +0100

    Remove sensitive PHP files from repo

--- connexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$machine = "localhost";
-$utilisateur = "root";
-$mot_de_passe = "";
-$bd = "traducode";
-$connexion = mysqli_connect($machine, $utilisateur, $mot_de_passe, $bd) or die('Problème de connexion au serveur');
-?>
